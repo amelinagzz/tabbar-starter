4baa57ad932c994069d627890a9b141f9c2d84d4
Adriana <melina.gzz@hotmail.com>
2020-02-17 23:10:10 -0800

    first commit

---
+++ TabBarDemo/Controller/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  TabBarDemo
+//
+//  Created by Adriana González Martínez on 2/17/20.
+//  Copyright © 2020 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+    
+    func setupViewControllers(){
+    }
+}
